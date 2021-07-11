26903ac682dbbb6457dcc74208bf4730831d25c8
lesha <leshak549@gmail.com>
2021-07-11 21:43:39 +0300

    add comment

--- src/main/java/main/dao/ModerationProfileDAOImpl.java
+++ src/main/java/main/dao/ModerationProfileDAOImpl.java
@@ -17,9 +17,22 @@ public class ModerationProfileDAOImpl implements ModerationProfileDAO{
     @Override
     public ResultResponse postStatus(ProfileResponse profileResponse) {
         User user = userRepository.findByEmail(profileResponse.email).get();
+        boolean moderation = false;
+        if(profileResponse.password != null){
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(12);
         user.setPassword((passwordEncoder.encode(profileResponse.getPassword())));
-        userRepository.save(user);
+        moderation = true;
+        }
+        if(profileResponse.name != null){
+            user.setName(profileResponse.name);
+            moderation = true;
+        }
+        if(profileResponse.email != null){
+            user.setEmail(profileResponse.email);
+            moderation = true;
+        }
+        if (moderation){
+        userRepository.save(user);}
         return ResultResponse.TRUE;
     }
 }
